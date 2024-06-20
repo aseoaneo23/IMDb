9b706f9607b1f7ce34115d40b4683c79dc648948
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2024-06-20 14:50:08 +0200

    solved on cypress config

--- cypress/pages/main_page.js
+++ cypress/pages/main_page.js
@@ -20,12 +20,7 @@ class MainPage {
     }
 
     visitIMDb = () => {
-        cy.visit('/', {
-            failOnStatusCode: false,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36'
-            }
-        });
+        cy.visit('/')
 
         //this.properties.getRejectCookiesBtn().click()
     }
