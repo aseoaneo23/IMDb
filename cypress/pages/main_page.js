34c5b534c4690eb945a5f0158f4f1996538883e7
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2024-06-20 09:51:10 +0200

    Syntax issue solved

--- cypress/pages/main_page.js
+++ cypress/pages/main_page.js
@@ -24,7 +24,8 @@ class MainPage {
             failOnStatusCode: false,
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36'
-          });
+            }
+        });
 
         this.properties.getRejectCookiesBtn().click()
     }
